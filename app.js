223b1cbea125d755fc9c4639292282bce0114488
Mike <munkyu0813@gmail.com>
2018-05-05 11:04:27 -0400

    add GET /posts, GET /posts/add, POST /posts routes

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Load Models
+const { Post } = require('./models/Post');
 
 const app = express();
 const port = 3000 || process.env.PORT;
 
-mongoose.connect('mongodb://localhost/BlogApp')
+// Connect to DB
+mongoose.connect('mongodb://localhost:27017/BlogApp')
   .then(() => console.log('---MongoDB Conneted---'))
   .catch(err => console.log(err));
 
@@ -15,6 +20,10 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+// BodyParser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Index Route
 app.get('/', (req, res) => {
   res.render('index');
@@ -23,6 +32,51 @@ app.get('/', (req, res) => {
 // About Route
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+// Post Index Page
+app.get('/posts', (req, res) => {
+  Post.find()
+    .sort({date:'desc'})  
+    .then((posts) => {
+      res.render('posts/index', {
+        posts
+      });
+    });
+});
+
+// Add Post Form
+app.get('/posts/add', (req, res) => {
+  res.render('posts/add');
+}); 
+
+app.post('/posts', (req, res) => {
+  let errors = [];
+
+  if (!req.body.title) {
+    errors.push({text: 'Please add a title'});
+  }
+  if (!req.body.text) {
+    errors.push({text: 'Please add some content'});
+  }
+
+  if (errors.length > 0) {
+    res.render('posts/add', {
+      errors,
+      title: req.body.title,
+      text: req.body.text
+    });
+  } else {
+    let body = {
+      title: req.body.title,
+      text: req.body.text
+    }
+    new Post(body)
+      .save()
+      .then(post => {
+        res.redirect('/posts');
+      });
+  }
 });
 
 app.listen(port, () => {
