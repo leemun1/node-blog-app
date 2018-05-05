af75ebb89b198c19f1cd856a076efb0fd147f92e
Mike <munkyu0813@gmail.com>
2018-05-05 08:26:33 -0400

    initial setup for templates

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ app.set('view engine', 'handlebars');
 // Index Route
 app.get('/', (req, res) => {
   res.render('index');
-});
+}); 
 
+// About Route
 app.get('/about', (req, res) => {
-  res.send('About');
+  res.render('about');
 });
 
 app.listen(port, () => {
