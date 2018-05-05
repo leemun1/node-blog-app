3bc70a72894f4cd59b8671abd1bcc71e6faf671f
Mike <munkyu0813@gmail.com>
2018-05-05 08:55:32 -0400

    configure mongoose and conenct to mongoDB locally

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000 || process.env.PORT;
+
+mongoose.connect('mongodb://localhost/BlogApp')
+  .then(() => console.log('---MongoDB Conneted---'))
+  .catch(err => console.log(err));
 
 // Handlebars
 app.engine('handlebars', exphbs({
