97e08983739ddfb712147c8d80a6cf812f532ba0
Mike <munkyu0813@gmail.com>
2018-05-05 21:01:29 -0400

    add login feature with passport.js

--- config/passport.js
+++ config/passport.js
@@ -7,6 +7,32 @@ const User = mongoose.model('User');
 
 module.exports = function (passport) {
   passport.use(new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
-    console.log(email);
+    // Match user
+    User.findOne({email})
+      .then(user => {
+        if (!user) {
+          return done(null, false, {message: 'Cannot find user. Please try a different email.'});
+        }
+
+        // Match password
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: 'Incorrect password. Please try again.' });            
+          }
+        });
+      });
   }));
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
 };
